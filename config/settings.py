cc7a39056c8172789269c5c3ea2ff4eb1babf034
Bart Kim <bart@xbarx.co.kr>
2021-03-06 01:50:40 +0900

    이전 commit 에서 추가되지 못한 파일 추가

--- config/settings.py
+++ config/settings.py
@@ -4,7 +4,7 @@ Django settings for djangonetes project.
 
 import os
 
-from config import db
+from config import database
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
