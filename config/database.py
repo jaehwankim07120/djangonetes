6e5783f74681ac62bcfe84c6149ee90199febcf4
Bart Kim <bart@xbarx.co.kr>
2021-03-06 03:54:26 +0900

    database 가 compose 를 사용하지 않을 때는 임시로 sqlite3(기본제공)을 이용하도록 변경

--- config/database.py
+++ config/database.py
@@ -16,14 +16,12 @@ if DJANGO_COMPOSE:
         }
     }
 else:
+    DB_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    
     # You define your db sysetm with online or other container without django-compose
-    setup = setup = {
+    setup = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ.get('DJANGO_DB_NAME', 'admin'),
-            'USER': os.environ.get('DJANGO_DB_USERNAME', 'admin'),
-            'PASSWORD': os.environ.get('DJANGO_DB_PASSWORD', 'adminadmin'),
-            'HOST': os.environ.get('DJANGO_DB_HOST', 'localhost'),
-            'PORT': os.environ.get('DJANGO_DB_PORT', '5432'),
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(DB_DIR, 'db.sqlite3'),
         }
     }
